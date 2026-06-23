9f5bcb26b82af029ce03b722bd2fb1c381d0fd13
bruhkookie <calebwongtech@gmail.com>
2026-06-22 19:15:20 -0700

    eval harness v2 added mrr comparision

--- eval_harness.py
+++ eval_harness.py
@@ -1,21 +1,42 @@
 from rag import retrieve, flatten_context
 import json 
 
+#we are going to use mean reciprocal rank (mrr) for considering distance weights for each of our retrieved documents
+# RR = 1 / rank (rank is how close it is to the query. technically not based on the numerical distance number but relative to all of the other documents fetched)
+#the final mrr is the average of our scores across the data set
 def get_metrics(k: int): 
     data = []
-    found = 0
+    mrr_sum = 0
+    hits = 0
+    hit_distances = []
+
     with open("golden_set.json", "r") as file: 
         data = json.load(file) #array of dictionaries
 
     for entry in data["golden_set"]: 
         raw_data = retrieve(entry["question"], k)
         documents = flatten_context(raw_data) #arr of dictionaries
-        for doc in documents: 
+        for rank, doc in enumerate(documents): 
             if doc["source"] in entry["expected_sources"]: 
-                found += 1
-                break
+                hits += 1
+                mrr_sum += (1.0 / (rank + 1)) #indexed starting at 0 so add 1
+                hit_distances.append(doc["distance"])
+                break #for this logic right now, we are only looking at the first matched doc. this algorithm can/should improve later on
 
-    return (found / len(data["golden_set"])) * 100 
+    hit_rate = hits / len(data["golden_set"])
+    avg_mrr = (mrr_sum / len(data["golden_set"]))
+    avg_hit_distance = 0 
+    if hit_distances: 
+        avg_hit_distance = sum(hit_distances) / len(hit_distances])
+    else: 
+        avg_hit_distance = 0
+    
+    return { 
+        "hit_rate": hit_rate, 
+        "mrr": avg_mrr, 
+        "avg_hit_distance": avg_hit_distance
+    }
+
 
 def trace_get_metrics(k: int): 
     data = []
@@ -36,17 +57,23 @@ def trace_get_metrics(k: int):
     return (found / len(data["golden_set"])) * 100 
 
 
+
+
 def test_range(start: int, end: int) -> tuple[int, int]:
     max_index = 0
-    max_percentage = 0
+    max_mrr = 0
     for i in range(start, end): 
-        percentage = trace_get_metrics(i) 
-        print(f"the accuracy for k = {i} is {percentage}&")
-        if percentage > max_percentage: 
-            max_percentage = percentage
+        results = get_metrics(i) 
+        print(f"the mrr for k = {i} is {results["mrr"]}")
+        print(f'the hit rate for k = {i} is {results["hit_rate"]}')
+        if results["mrr"] > max_mrr: 
+            max_mrr = results["mrr"]
             max_index = i
-    return max_index, max_percentage
+    return max_index, max_mrr
+
+
+
 
 if __name__ == '__main__':
-    k, optimal_percentage = test_range(1, 20) 
-    print(f'the most optimal k-index is {k} which has an accuracy of {optimal_percentage}%')
+    k, optimal_mrr = test_range(1, 20) 
+    print(f'the most optimal k-index is {k} which has an mrr of {optimal_mrr}')
